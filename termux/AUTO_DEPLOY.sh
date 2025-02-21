222a83f1816bab0e814839dc26b4083758e09b6f
VERAN-TECH-HUB <veranmaina685@gmail.com>
2025-02-21 18:26:53 +0100

    Update AUTO_DEPLOY.sh

--- termux/AUTO_DEPLOY.sh
+++ termux/AUTO_DEPLOY.sh
@@ -7,12 +7,6 @@ if [ ! -f keys/debug.keystore ]; then
     keytool -genkey -v -keystore keys/debug.keystore -storepass android -alias androiddebugkey -keypass android -dname "CN=Android Debug"  
 fi  
 
-# Install tools  
-pkg install apktool openjdk-17 php ngrok -y  
-
-# Start server  
-./termux/SERVER_KILLER.sh  
-
 # Build APK  
 apktool b app -o apk_out/FB_Killer.apk  
 
@@ -21,4 +15,4 @@ cd apk_out
 zipalign -v 4 FB_Killer.apk FB_Signed.apk  
 apksigner sign --ks ../keys/debug.keystore --ks-pass pass:android FB_Signed.apk  
 
-echo "[+] APK READY: apk_out/FB_Signed.apk"  
+echo "[+] APK READY: $PWD/FB_Signed.apk"  
